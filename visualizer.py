42024f63056e579b3bab8be4da3943c9bb0ac5b5
Terry Fang <tlf240000@utdallas.edu>
2026-05-10 10:41:39 -0500

    To do list

---
+++ visualizer.py
@@ -0,0 +1,19 @@
+import sys, librosa, numpy, pygame
+
+#screen
+
+#music player
+
+#mp3 audio data
+
+#mp3 metadata
+
+#visualizer
+
+#audio controller
+
+#image display
+
+#text display
+
+#main loop
