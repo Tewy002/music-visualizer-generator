9470c0cbb44ab498ca4020b7edf0be92ece5c12a
Terry Fang <tlf240000@utdallas.edu>
2026-05-10 20:45:38 -0500

    Bar visualizer implementation and rendering

--- visualizer.py
+++ visualizer.py
@@ -1,6 +1,7 @@
 import sys, numpy, pygame
 
 from pydub import AudioSegment
+from scipy.fftpack import dct
 
 BARS = 50 # number of bars
 HEIGHT = 100 # height of bars
@@ -14,7 +15,7 @@ clock = pygame.time.Clock()
 #screen
 pygame.init()
 pygame.mixer.init()
-screen = pygame.display.set_mode([BARS * WIDTH, HEIGHT]) 
+screen = pygame.display.set_mode([BARS * WIDTH, + HEIGHT]) 
 pygame.display.set_caption('Audio Visualizer')
 
 #music player
@@ -24,11 +25,44 @@ pygame.mixer.music.set_endevent()
 status = "playing"
 
 #mp3 audio data
+audio = AudioSegment.from_file(file_name)
+framerate = audio.frame_rate
+samples = numpy.array(audio.get_array_of_samples())
+wave_data = samples.reshape((-1, 2)).T
+frames = wave_data.shape[1]
 
 #mp3 metadata
 
+n = frames
 #visualizer
+    #make the visualizer
+    #audio frequency mapping
+    #make it look good lmao
 
+temp = []
+
+def visualizer(n):
+    n = int(n)
+    h = abs(dct(wave_data[0][frames - n:frames - n + BARS *2]))
+    h = [min(HEIGHT,int(i **(1 / 2.5) * HEIGHT / 100)) for i in h]
+    draw_bars(h)
+
+def render(status):
+    global n
+    if status == "stopped":
+        n = frames
+        return
+    else:
+        n -= framerate/FPS
+        if n > 0:
+            visualizer(n)
+
+def draw_bars(h):
+    bars = []
+    for i in h:
+        bars.append([len(bars) * WIDTH,50 + HEIGHT-i,WIDTH - 1,i])
+    for i in bars:
+        pygame.draw.rect(screen,[255,255,255],i,0)
 #audio controller
 
 #image display
@@ -43,5 +77,5 @@ while True:
 
     screen.fill((0,0,0))
     clock.tick(FPS)
-
+    render(status)
     pygame.display.update()
