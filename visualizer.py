78dd570e0fc343c64ee42f13e18d9b21073dd145
Terry Fang <tlf240000@utdallas.edu>
2026-05-10 20:15:36 -0500

    Initialize variables, music player and display screen

--- visualizer.py
+++ visualizer.py
@@ -1,8 +1,27 @@
-import sys, librosa, numpy, pygame
+import sys, numpy, pygame
+
+from pydub import AudioSegment
+
+BARS = 50 # number of bars
+HEIGHT = 100 # height of bars
+WIDTH = 12 # width of bars
+FPS = 60
+
+file_name = sys.argv[1]
+status = "stopped"
+clock = pygame.time.Clock()
 
 #screen
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode([BARS * WIDTH, HEIGHT]) 
+pygame.display.set_caption('Audio Visualizer')
 
 #music player
+pygame.mixer.music.load(file_name)
+pygame.mixer.music.play()
+pygame.mixer.music.set_endevent()
+status = "playing"
 
 #mp3 audio data
 
@@ -17,3 +36,12 @@ import sys, librosa, numpy, pygame
 #text display
 
 #main loop
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+    screen.fill((0,0,0))
+    clock.tick(FPS)
+
+    pygame.display.update()
